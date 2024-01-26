05b971b9a54c94add0b3a5407dea8e0b42bb7b23
Gabe <gabriel.velasquez0116@gmail.com>
2024-01-25 19:04:30 -0800

    fixed error where it could not find files

--- app/index.js
+++ app/index.js
@@ -1,10 +1,25 @@
-import  {View, Text} from 'react-native';
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { COLORS, icons, images, SIZES } from '../constants';
+import { 
+    Nearbyjobs, 
+    Popularjobs, 
+    ScreenHeaderBtn, 
+    Welcome } 
+from '../components';
 
 const Home=()=>{
+    const router=useRouter();
+
     return(
-        <View>
-            <Text>Home Screen</Text>
-        </View>
+        <SafeAreaView style={{flex:1, backgroundColor:COLORS.lightWhite}}>
+            <Stack.Screen
+                options={{
+                    headerStyle:{backgroundColor:COLORS.lightWhite},
+                }}
+            /> 
+        </SafeAreaView>
     )
 }
 
